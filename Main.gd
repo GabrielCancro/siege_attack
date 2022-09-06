023e8c853c2606db05d830ea06dd760c26963e44
gabrielcancro <gabrielcancro@3ogs.com.ar>
2022-09-06 12:09:13 -0300

    update

--- Main.gd
+++ Main.gd
@@ -8,55 +8,42 @@ var current_rock = null
 var shadow_rock_1 = null
 var shadow_rock_2 = null
 var power = 0
-var isReady = true
+var state = "READY" #SHOWING #RELOAD
 
 func _ready():
 	randomize()
 	InputManager.connect("im_click",self,"onClick")
 	$ShadowsRocks.connect("timeout",self,"add_shadow_rock")
 	$Trabuc.connect("on_reload",self,"create_new_trabuc_rock")
+	$UI/lb_press.visible = false
 	update_ui()
 	create_new_trabuc_rock()
-	
 
 func _process(delta):
-	if Input.is_action_just_pressed("ui_up"):
-		angle = min(angle+5,60)		
-		update_ui()
-	if Input.is_action_just_pressed("ui_down"):
-		angle = max(angle-5,10)
-		update_ui()
-	if Input.is_action_just_pressed("ui_right"):
-		force = min(force+20,600)
-		update_ui()
-	if Input.is_action_just_pressed("ui_left"):
-		force = max(force-20,400)
-		update_ui()
-#	if Input.is_action_just_pressed("ui_accept"):
-#		onClick(null)
-	if Input.is_action_pressed("ui_accept"):
-		if isReady: power = min(power+.2,20)
-	if Input.is_action_just_released("ui_accept"): 
-		if power != 0 && isReady: 
-			$Trabuc.shoot(power)
-			power = 0
-			isReady = false
-			yield(get_tree().create_timer(2),"timeout")
-			create_new_trabuc_rock()
-			yield(get_tree().create_timer(1),"timeout")
-			isReady = true
-	$UI/lb_pow.text = str(floor(power))
-	if current_rock: 
-		$Camera2D.position.x = current_rock.position.x+150
-		print(current_rock.linear_velocity.length())
-		if current_rock.linear_velocity.length() < 2: 
-			current_rock = null
-	else: 
-		$Camera2D.position.x = 500
+	if Input.is_action_pressed("ui_right"): $Trabuc.move(+1)
+	if Input.is_action_pressed("ui_left"): $Trabuc.move(-1)
+	if Input.is_action_pressed("ui_accept") && state=="READY":
+		power = min(power+.2,20)
+		$Trabuc.set_power(power)
+	if Input.is_action_just_released("ui_accept") && state=="READY": 
+		$Trabuc.shoot(power)
+		power = 0
+		$Trabuc.set_power(power)
+		state="SHOOTED"
+		yield(get_tree().create_timer(2),"timeout")
+		state="SHOWED"
+		$UI/lb_press.visible = true
+	if Input.is_action_just_released("ui_accept") && state=="SHOWED": 
+		state="READY"
+		create_new_trabuc_rock()
+		$UI/lb_press.visible = false
+	$UI/lb_state.text = state
+	if state=="READY": $Camera2D.position.x = $Trabuc.position.x + 200
+	else: $Camera2D.position.x = current_rock.position.x+150
 
 func create_new_trabuc_rock():
 	var b = ROCK_SCENE.instance()
-	b.position = $Trabuc.position
+	b.position = $Trabuc/Arm/ReloadPoint.global_position
 	add_child(b)
 	current_rock = b
 
@@ -65,7 +52,7 @@ func add_enemy(val):
 	update_ui()
 
 func update_ui():
-	$UI/lb_fg.text = str(force)+"f\n"+str(angle)+"*"
+	#$UI/lb_fg.text = str(force)+"f\n"+str(angle)+"*"
 	$UI/lb_enemies.text = str(enemies_counter)
 	if enemies_counter <=0: $UI/lb_end_game.visible = true
 
--- Trabuc.gd
+++ Trabuc.gd
@@ -1,15 +1,17 @@
 extends Node2D
 
 func _ready():
-	set_down(false)
-
-func set_down(val):
 	pass
 
 func _process(delta):
 	if $Arm.angular_velocity <= 0: $Arm.angular_velocity = -1
-	$Label.text = str( floor($Arm.rotation_degrees) )
 	
-	
+func move(val):
+	if(abs($Base.position.x+val*10)>100): return
+	$Base.linear_velocity = Vector2(val,0)*60
+
+func set_power(val):
+	$Base/Label.text = str( floor( val ) )
+
 func shoot(val):
 	$Arm.angular_velocity = 15+val
