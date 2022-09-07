b00f2b8c3b750f4a5f0fa3fce896d179e588b230
gabrielcancro <gabrielcancro@3ogs.com.ar>
2022-09-07 10:33:29 -0300

    update

--- Main.gd
+++ Main.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
-var ROCK_SCENE = preload("res://blocks/Rock1.tscn")
+var ROCKS = {
+	"standar": preload("res://blocks/Rock_standar.tscn"),
+	"small": preload("res://blocks/Rock_small.tscn"),
+}
+var rocks_array = ["standar","small","standar","small","standar"]
 var enemies_counter = 0
 var force = 500
 var angle = 45
@@ -10,12 +14,16 @@ var shadow_rock_2 = null
 var power = 0
 var state = "READY" #SHOWING #RELOAD
 
+
 func _ready():
 	randomize()
 	InputManager.connect("im_click",self,"onClick")
 	$Trabuc.connect("on_reload",self,"create_new_trabuc_rock")
 	$UI/lb_press.visible = false
 	create_new_trabuc_rock()
+	$UI/Tween.interpolate_property($UI/Telon,"modulate",$UI/Telon.modulate,Color(1,1,1,0),1,Tween.TRANS_QUAD,Tween.EASE_IN)
+	$UI/Tween.start()
+	$UI/Telon.visible = true
 
 func _process(delta):
 	if Input.is_action_pressed("ui_right"): $Trabuc.move(+1)
@@ -37,16 +45,48 @@ func _process(delta):
 		$UI/lb_press.visible = false
 	$UI/lb_state.text = state
 	if state=="READY": $Camera2D.position.x = $Trabuc.position.x + 200
-	else: $Camera2D.position.x = current_rock.position.x+150
+	elif current_rock: $Camera2D.position.x = current_rock.position.x+150
 
 func create_new_trabuc_rock():
-	var b = ROCK_SCENE.instance()
-	b.position = $Trabuc/Arm/ReloadPoint.global_position
-	add_child(b)
-	current_rock = b
+	if enemies_counter<=0: endGame(true)
+	elif rocks_array.size()==0: endGame(false)
+	var next_rock = rocks_array.pop_front()
+	$UI/lb_shoots.text = str(rocks_array.size())
+	current_rock = null
+	if next_rock=="standar":
+		var b = ROCKS.standar.instance()
+		b.position = $Trabuc/Arm/ReloadPoint.global_position
+		add_child(b)
+		current_rock = b
+	if next_rock=="small":
+		var b = ROCKS.small.instance()
+		b.position = $Trabuc/Arm/ReloadPoint.global_position - Vector2(0,0)
+		add_child(b)
+		b = ROCKS.small.instance()
+		b.position = $Trabuc/Arm/ReloadPoint.global_position - Vector2(-5,-5)
+		add_child(b)
+		b = ROCKS.small.instance()
+		b.position = $Trabuc/Arm/ReloadPoint.global_position - Vector2(5,-5)
+		add_child(b)
+		current_rock = b
 
 func add_enemy(val):
 	enemies_counter += val
 	$UI/lb_enemies.text = str(enemies_counter)
+	if enemies_counter<=0: endGame(true)
 
+func endGame(win=false):
+	set_process(false)
+	if win: 
+		$UI/lb_end_game.text = "HAS GANADO!!"
+		$UI/lb_end_game.modulate = Color(1,1,.5,1)
+	else:
+		$UI/lb_end_game.text = "PERDISTE!!"
+		$UI/lb_end_game.modulate = Color(1,.5,.5,1)
+	$UI/lb_end_game.visible = true
+	yield(get_tree().create_timer(2),"timeout")
+	$UI/Tween.interpolate_property($UI/Telon,"modulate",$UI/Telon.modulate,Color(1,1,1,1),3,Tween.TRANS_QUAD,Tween.EASE_IN)
+	$UI/Tween.start()
+	yield(get_tree().create_timer(3),"timeout")
+	get_tree().reload_current_scene()
 
---
+++ scripts/Tuto.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var isStoped = false
+var read_time = 5
+var cFrom =  Color(.7,.7,.7,0)
+var cTo = Color(.7,.7,.7,1)
+
+var texts = [
+	"Manetené [A] para cargar,\nSoltalo para disparar.",
+	"Usá los direccionales para moverte\na la izquierda y derecha.",
+	"Asediá el castillo y aplastá\na todos los soldados."
+]
+
+func _ready():
+	$Label.visible = false
+	yield(get_tree().create_timer(2),"timeout")
+	next_tuto(0)
+
+func next_tuto(index=0):
+	if isStoped: return
+	if index >= texts.size(): return
+	$Label.visible = true
+	$Label.modulate = cFrom
+	$Label.text = texts[index]
+	$Tween.interpolate_property($Label,"modulate",cFrom,cTo,.5,Tween.TRANS_QUAD,Tween.EASE_IN)
+	$Tween.start()
+	yield(get_tree().create_timer(read_time),"timeout")
+	$Tween.interpolate_property($Label,"modulate",cTo,cFrom,.5,Tween.TRANS_QUAD,Tween.EASE_IN)
+	$Tween.start()
+	yield(get_tree().create_timer(1),"timeout")
+	next_tuto(index+1)
